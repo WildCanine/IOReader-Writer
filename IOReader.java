394736ee5cdf0e21e24d721e41181e09ebc6c1ca
WildCanine <43619473+WildCanine@users.noreply.github.com>
2019-01-15 12:54:42 -0700

    Add files via upload

---
+++ IOReader.java
@@ -0,0 +1,20 @@
+package a;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class IOReader {
+	public static void main(String[] args) {
+		try {
+			FileReader fr=new FileReader("Kennedy.txt");
+			BufferedReader br=new BufferedReader(fr);
+			String str;
+			while((str=br.readLine())!=null) {
+				System.out.println(str+"/n");
+			}
+			br.close();
+		}
+		catch (IOException e) {
+			System.out.println("IO error:"+e);
+		}
+	}
+}
